3e0c07ad5ad15c0d01f9225658cbf606d519ddfc
Bryne <228299496+b1yn3@users.noreply.github.com>
2025-12-03 17:56:07 +0800

    FINISHED

--- script.js
+++ script.js
@@ -6,31 +6,50 @@ container.style.width = '600px';
 container.style.display = 'flex';
 container.style.flexDirection = 'column';
 
-const btn = document.querySelector('button');
-
-btn.addEventListener('onclick', () => {
+function promptB(){
     
-})
-
-
-for(let i =0; i < 16; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add("row")
-    rowDiv.style.backgroundColor = 'white';
-    rowDiv.style.flex = 1;
-    rowDiv.style.display = 'flex';
-    for(let j =0; j < 16; j++){
-        const colDiv = document.createElement('div');
-        colDiv.classList.add("col")
-        colDiv.style.flex = 1;
-        colDiv.style.backgroundColor = 'white';
-        colDiv.style.border = '.5px solid gray';
-        colDiv.addEventListener('mouseenter', (event) =>{
-            colDiv.style.backgroundColor = 'purple'; 
-        })
-        rowDiv.appendChild(colDiv);
+    let newRow = parseInt(prompt("Enter the number of rows: "))
+    let newCol = parseInt(prompt("Enter the number of columns: "))
+    if(checkIf(newRow, newCol)){
+        container.innerHTML = ''
+        makeSquare(newRow, newCol);
         
+    } else {
+        alert("Invalid input! Please enter a number between 1 and 100.");
+        return;
     }
-    container.appendChild(rowDiv)
 }
 
+function checkIf(newRow, newCol){
+    if(!isNaN(newRow) && !isNaN(newCol) && newRow > 0 && newRow <= 100 && newCol > 0 && newCol <= 100){
+        return true
+    } 
+    return false
+}
+
+function makeSquare(rowNum, colNum){
+
+    for(let i =0; i < rowNum; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add("row")
+        rowDiv.style.backgroundColor = 'white';
+        rowDiv.style.flex = 1;
+        rowDiv.style.display = 'flex';
+        for(let j =0; j < colNum; j++){
+            const colDiv = document.createElement('div');
+            colDiv.classList.add("col")
+            colDiv.style.flex = 1;
+            colDiv.style.backgroundColor = 'white';
+            colDiv.style.border = '.5px solid gray';
+            colDiv.addEventListener('mouseenter', (event) =>{
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                colDiv.style.backgroundColor = "#" + randomColor; 
+            })
+            rowDiv.appendChild(colDiv);
+            
+        }
+        container.appendChild(rowDiv)
+    }
+}
+
+makeSquare(16,16);
